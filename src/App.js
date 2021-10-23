a348aa8b707a6437428a6e5422b440a407846488
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-23 17:47:58 +0530

    added aos by npm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Aos from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
   return (
     <div className="App">
       <Home />
